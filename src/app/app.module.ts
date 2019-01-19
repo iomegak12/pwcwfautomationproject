5bfe68781bcf0e5384d4919a30a389190a0509e9
iomegak12 <jd.ramkumar@gmail.com>
2019-01-19 16:22:37 +0530

    final commit of day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TitlecasePipe } from './titlecase.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TitlecasePipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
